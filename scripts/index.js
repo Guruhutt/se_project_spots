cdf27f7cd495c988338513cd7fb637a81e823d92
Aaron <dogking189@gmail.com>
2025-03-10 00:56:17 -0500

    atage]

--- scripts/index.js
+++ scripts/index.js
@@ -26,13 +26,32 @@ const initialCards = [
 ];
 
 const profilEditButton = document.querySelector(".profile__button-edit");
-const editModal = document.querySelector("#edit-profile-modal");
-const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const profileName = document.querySelector(".profile__title");
 const profileDes = document.querySelector(".profile__description");
-const modalInputName = editModal.querySelector("#profile-name-input");
+
+const editModal = document.querySelector("#edit-profile-modal");
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const editmodalform = editModal.querySelector(".modal__form");
+
 const modalInputDes = editModal.querySelector("#profile-description-input");
+const modalInputName = editModal.querySelector("#profile-name-input");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__description");
+  const cardImages = cardElement.querySelector(".card__image");
+
+  cardNameEl.textContent = data.name;
+  cardImages.src = data.link;
+
+  return cardElement;
+}
 
 function openModal() {
   modalInputName.value = profileName.textContent;
@@ -55,3 +74,8 @@ function handleEditFormSubmit(evt) {
 profilEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editmodalform.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
